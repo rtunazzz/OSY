f05b9f76fcd9cdea213c06942db0f21cea55f6cc
Artur Hnát <hnatartu@fit.cvut.cz>
2021-05-17 15:39:23 +0200

    [OSY-1] - Implement majority of the methods

--- progtest_solver/solution.cpp
+++ progtest_solver/solution.cpp
@@ -39,7 +39,8 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
-/* ---------------- WORKFLOW ----------------
+/*
+**************************** WORKFLOW ****************************
 - Number of threads spawned is specified by the Start method
 - When Ship gets called, Sales threads need to:
     - Get destination
@@ -53,14 +54,42 @@ using namespace std;
 - Once we have quotes from all customers, we need to pick which cargo will and which won't
   go on the Ship. Worker threads will be doing that, not Sales threads.
 - Once that's decided and calculated, CShip::Load gets called.
+
+**************************** TIPS ****************************
+ - Make sure we're serving multiple ships & customers at once
+
+**************************** TODO ****************************
+
 */
 
-/* ---------------- TIPS ----------------
- - Make sure we're serving multiple ships & customers at once
-*/
+class JobInfo {
+   public:
+    bool forWorker = false;
+    bool forSales = false;
+    bool done = false;
+    function<void()> job;
+
+    JobInfo(bool _forWorker, bool _forSales, bool _done, function<void()> _job) : forWorker(_forWorker), forSales(_forSales), done(_done), job(_job){};
+};
 
 class CCargoPlanner {
    private:
+    vector<thread> m_Workers;
+    vector<thread> m_Sales;
+
+    vector<ACustomer> m_Customers;
+
+    queue<JobInfo> m_JobQueue;
+    mutex m_QMtx;
+    condition_variable m_QCond;
+
+    void worker(int index);
+    void salesman(int index);
+
+    void handleDestination(AShip& ship);
+    void handleQuote(const ACustomer& customer, AShip& ship);
+    void handleLoad(const vector<CCargo>& cargo, AShip& ship);
+
    public:
     static int SeqSolver(const vector<CCargo>& cargo,
                          int maxWeight,
@@ -74,6 +103,101 @@ class CCargoPlanner {
     void Ship(AShip ship);
 };
 
+void CCargoPlanner::worker(int index) {
+    cout << "[" << index << "]"
+         << " Worked started." << endl;
+
+    while (true) {
+        unique_lock<mutex> qlock(m_QMtx);
+        m_QCond.wait(qlock, [this] { return (!m_JobQueue.empty()) && (m_JobQueue.front().forWorker); });
+
+        JobInfo jobData = m_JobQueue.front();
+        cout << "[" << index << "] W - " << m_JobQueue.size() << " - Found a job, done: " << (jobData.done ? "YES" : "Not yet...") << endl;
+        if (jobData.done) {
+            break;
+        }
+        // cout << "[" << index << "] W - " << m_JobQueue.size() << " - Job assigned" << endl;
+        m_JobQueue.pop();
+        // unlock the queue since we don't need it locked anymore
+        qlock.unlock();
+
+        // do the job
+        jobData.job();
+        // cout << "[" << index << "] S - Job done." << endl;
+    }
+
+    cout << "[" << index << "]"
+         << " Salesman ended." << endl;
+}
+
+void CCargoPlanner::salesman(int index) {
+    cout << "[" << index << "]"
+         << " Salesman started." << endl;
+
+    while (true) {
+        unique_lock<mutex> qlock(m_QMtx);
+        m_QCond.wait(qlock, [this] { return (!m_JobQueue.empty()) && (m_JobQueue.front().forSales); });
+
+        JobInfo jobData = m_JobQueue.front();
+        cout << "[" << index << "] S - " << m_JobQueue.size() << " - Found a job, done: " << (jobData.done ? "YES" : "Not yet...") << endl;
+        if (jobData.done) {
+            break;
+        }
+        // cout << "[" << index << "] S - " << m_JobQueue.size() << " - Job assigned" << endl;
+        m_JobQueue.pop();
+
+        // unlock the queue since we don't need it locked anymore
+        qlock.unlock();
+
+        // do the job
+        jobData.job();
+        // cout << "[" << index << "] S - Job done." << endl;
+    }
+
+    cout << "[" << index << "]"
+         << " Salesman ended." << endl;
+}
+
+void CCargoPlanner::handleDestination(AShip& ship) {
+    cout << "Adding jobs to quote " << m_Customers.size() << " customers for destination " << ship->Destination() << endl;
+    // Create a job to handle quoting for each customer
+    for (auto const& it : m_Customers) {
+        unique_lock<mutex> qlock(m_QMtx);
+        m_JobQueue.emplace(
+            /* forWorker */ false,
+            /* forSales */ true,
+            /* done */ false,
+            /* job */ bind(&CCargoPlanner::handleQuote, this, it, ship));
+        cout << m_JobQueue.size() << " Job handleQuote to " << ship->Destination() << " added" << endl;
+        m_QCond.notify_one();
+    }
+}
+
+void CCargoPlanner::handleQuote(const ACustomer& customer, AShip& ship) {
+    cout << "Quoting a customer for destination " << ship->Destination() << endl;
+    vector<CCargo> quotedCargo;
+    customer->Quote(ship->Destination(), quotedCargo);
+
+    unique_lock<mutex> qlock(m_QMtx);
+    m_JobQueue.emplace(
+        /* forWorker */ true,
+        /* forSales */ false,
+        /* done */ false,
+        /* job */ bind(&CCargoPlanner::handleLoad, this, quotedCargo, ship));
+
+    cout << m_JobQueue.size() << " Job handleLoad to " << ship->Destination() << " added" << endl;
+
+    m_QCond.notify_one();
+}
+
+void CCargoPlanner::handleLoad(const vector<CCargo>& cargo, AShip& ship) {
+    // cout << "Handling load for destination: " << ship->Destination() << endl;
+    vector<CCargo> load;
+    int fees = SeqSolver(cargo, ship->MaxWeight(), ship->MaxVolume(), load);
+    cout << "Loading ship to '" << ship->Destination() << "' with fees: " << fees << endl;
+    ship->Load(load);
+}
+
 // Tests our algorithm for loading ships
 int CCargoPlanner::SeqSolver(const vector<CCargo>& cargo,
                              int maxWeight,
@@ -86,26 +210,71 @@ int CCargoPlanner::SeqSolver(const vector<CCargo>& cargo,
 // Creates a number of sales and worker threads specified
 void CCargoPlanner::Start(int sales,
                           int workers) {
+    cout << "*********************************" << endl;
+    cout << "Starting " << sales << " sales and " << workers << " workers" << endl;
+    cout << "*********************************" << endl;
     // initialize salesThreads and workThreads, those will be waiting for ships
+    for (int i = 0; i < sales; i++) {
+        m_Sales.emplace_back(&CCargoPlanner::salesman, this, i);
+    }
+
+    for (int i = 0; i < workers; i++) {
+        m_Workers.emplace_back(&CCargoPlanner::worker, this, i);
+    }
 }
 
 void CCargoPlanner::Stop(void) {
     // when this gets called, there won't be any more ships
     //  = fill all ships we currently have
     //  and join threads back into the main thread
+    // wait till the queue is empty
+    cout << "Waiting for queue to be empty..." << endl;
+    unique_lock<mutex> qlock(m_QMtx);
+    m_QCond.wait(qlock, [this] { return (m_JobQueue.empty()); });
+    cout << m_JobQueue.size() << " Queue is empty!" << endl;
+    // add a new end "job" for all workers
+    m_JobQueue.emplace(
+        /* forWorker */ true,
+        /* forSales */ true,
+        /* done */ true,
+        /* job */ nullptr);
+
+    qlock.unlock();
+
+    // wake all threads
+    m_QCond.notify_all();
+
+    for (thread& t : m_Sales) {
+        t.join();
+    }
+
+    for (thread& t : m_Workers) {
+        t.join();
+    }
 }
 
 // registers a customer that gets passed in
 void CCargoPlanner::Customer(ACustomer customer) {
+    m_Customers.push_back(customer);
 }
 
 // Adds a ship
 void CCargoPlanner::Ship(AShip ship) {
     // return from this method should be instant (no computation)
 
-    // add a ship
-
     // handle transfer of the ship to worker & sales threads
+    // Get destination
+    // Afterwards ask all registered customers for a list of items
+    //      (will be done in the handleDestination method)
+    unique_lock<mutex> qlock(m_QMtx);
+    cout << m_JobQueue.size() << " Adding a ship to: " << ship->Destination() << endl;
+    m_JobQueue.emplace(
+        /* forWorker */ false,
+        /* forSales */ true,
+        /* done */ false,
+        /* job */ bind(&CCargoPlanner::handleDestination, this, ship));
+    m_QCond.notify_one();
+    // cout << "Job for destination " << ship->Destination() << " added & notified one." << endl;
 }
 
 //-------------------------------------------------------------------------------------------------
